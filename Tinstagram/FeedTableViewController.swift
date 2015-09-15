61bd49adf9a6740b59cf732d5cfa54f604fb795d
minhthanh91sg <minhthanh91sg@gmail.com>
2015-09-15 17:47:18 +1000

    made feed works

--- Tinstagram/FeedTableViewController.swift
+++ Tinstagram/FeedTableViewController.swift
@@ -14,6 +14,9 @@ class FeedTableViewController: UITableViewController {
     
     var feedFiles = [PFFile]()
     
+    var feedUser = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,16 +28,19 @@ class FeedTableViewController: UITableViewController {
     
     override func viewDidAppear(animated: Bool) {
         feedFiles.removeAll(keepCapacity: false)
+        feedUser.removeAll(keepCapacity: false)
         if let followingArray: [String] = currentUser.objectForKey("following") as? [String]{
-            println("this is the number of following people " + "\(followingArray.count)" )
             var imageQuery = PFQuery(className: "Image")
             imageQuery.whereKey("userId", containedIn: followingArray)
             imageQuery.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]?, error: NSError?) -> Void in
                 if error == nil{
                     if let objects = objects{
                         for object in objects{
+                            self.feedUser.append(object.objectForKey("userId") as! String)
                             self.feedFiles.append(object.objectForKey("image") as! PFFile)
+                            self.tableView.reloadData()
                         }
+                        println("view: \(self.feedUser.count)")
                     }
                 }
             
@@ -54,68 +60,37 @@ class FeedTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        println("Test2:\(feedFiles.count)")
+        return feedFiles.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCellWithIdentifier("feedcell", forIndexPath: indexPath) as! feedCell
+        cell.username.text = self.feedUser[indexPath.row]
+        println("test:\(self.feedUser[indexPath.row])")
+        self.feedFiles[indexPath.row].getDataInBackgroundWithBlock({ (imageData: NSData?, error: NSError?) -> Void in
+            if error == nil{
+                let image = UIImage(data: imageData!)
+                cell.imageFeed.image = image
+            }
+        })
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+class feedCell: UITableViewCell{
+    
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var imageFeed: UIImageView!
+}
+
+
