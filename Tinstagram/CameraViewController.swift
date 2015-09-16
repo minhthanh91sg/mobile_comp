d8f532e013f0d896a38f84d93bad850592f9ef2f
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-09-16 15:45:53 +1000

    Add CameraViewController to Main Tab Controller

---
+++ Tinstagram/CameraViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CameraViewController.swift
+//  Tinstagram
+//
+//  Created by Matheu Malo on 16/09/2015.
+//  Copyright (c) 2015 Thanh Pham. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
