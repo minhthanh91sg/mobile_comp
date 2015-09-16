33c509f5bd4d5030f138a8814ab1fde16016a3cf
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-09-16 20:37:48 +1000

    Add basic camera app.

--- Tinstagram/CameraViewController.swift
+++ Tinstagram/CameraViewController.swift
@@ -8,28 +8,49 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var pickedImageView: UIImageView!
+    
+    @IBAction func OpenCamera(sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            
+            imagePicker.sourceType = .Camera
+            imagePicker.editing = true
+            //imagePicker.showsCameraControls = true
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        else {
+            println("camera not available")
+            //put an alert alert message here ?
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    // Puts the taken photo in the image view
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pickedImageView.contentMode = .ScaleAspectFit
+            pickedImageView.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+        
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 }
