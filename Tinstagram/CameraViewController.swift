063b19cca5c60d33d3c2c4e4d1ac943dd0a44172
Ivan M. Malo <imalo@student.unimelb.edu.au>
2015-09-17 19:22:24 +1000

    Add custom shutter button and cancel button

--- Tinstagram/CameraViewController.swift
+++ Tinstagram/CameraViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+@IBDesignable
 
-class CameraViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+class CameraViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIGestureRecognizerDelegate {
+    
+    @IBInspectable var shutterButtonColor: UIColor = UIColor.blueColor()
+    @IBInspectable var cancelButtonColor: UIColor = UIColor.grayColor()
+
     
     let imagePicker = UIImagePickerController()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +32,44 @@ class CameraViewController: UIViewController,UIImagePickerControllerDelegate,UIN
         
         if UIImagePickerController.isSourceTypeAvailable(.Camera){
             
+            
+            
             imagePicker.sourceType = .Camera
-            imagePicker.editing = true
-            //imagePicker.showsCameraControls = true
+            imagePicker.allowsEditing = true
+            imagePicker.showsCameraControls = false
+            
+            
+            let overlayView = UIView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
+            overlayView.backgroundColor = UIColor.clearColor()
+            
+            /* Custom Camera Shutter button */
+            let shutterButton = UIView(frame: CGRectMake(self.view.frame.width/2 - 40, self.view.frame.height - 80, 80, 80))
+            shutterButton.layer.cornerRadius = 40
+            shutterButton.userInteractionEnabled = true
+            shutterButton.backgroundColor = shutterButtonColor
+            println(shutterButton)
+            overlayView.addSubview(shutterButton)
+            overlayView.bringSubviewToFront(shutterButton)
+            
+            
+            let recognizer = UITapGestureRecognizer(target: self, action:Selector("handleSnapTap:"))
+            recognizer.delegate = self
+            shutterButton.addGestureRecognizer(recognizer)
+            
+            /* Custom Camera Cancel button */
+            
+            let cancelButton = UIView(frame: CGRectMake(10, 10, 44, 44))
+            cancelButton.userInteractionEnabled = true
+            cancelButton.backgroundColor = cancelButtonColor
+            println(cancelButton)
+            overlayView.addSubview(cancelButton)
+            overlayView.bringSubviewToFront(cancelButton)
+            
+            let cancelRecognizer = UITapGestureRecognizer(target: self, action: "handleCancelTap:")
+            cancelRecognizer.delegate = self
+            cancelButton.addGestureRecognizer(cancelRecognizer)
+            
+            imagePicker.cameraOverlayView = overlayView
             
             self.presentViewController(imagePicker, animated: true, completion: nil)
         }
@@ -34,6 +77,18 @@ class CameraViewController: UIViewController,UIImagePickerControllerDelegate,UIN
             println("camera not available")
             //put an alert alert message here ?
         }
+    }
+    
+    func handleSnapTap(recognizer: UITapGestureRecognizer) {
+        println("Take picture")
+        imagePicker.takePicture()
+        
+    }
+    
+    func handleCancelTap(recognizer: UITapGestureRecognizer){
+        println("Cancel")
+        dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
     
