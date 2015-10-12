e3f5660bb1abee7d0eea9bfc36b4e8e600390cac
lfan1776 <lfan1776@gmail.com>
2015-10-12 17:47:18 +1100

    add activity segments

---
+++ Tinstagram/ActivityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityViewController.swift
+//  Tinstagram
+//
+//  Created by Lu Fan on 12/10/2015.
+//  Copyright (c) 2015 Thanh Pham. All rights reserved.
+//
+
+import Cocoa
+
+class ActivityViewController: UIViewController {
+
+}
