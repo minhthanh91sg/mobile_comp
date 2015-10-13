07ead0d3f5e5037d97836ff2739e5b6c26db644c
lfan1776 <lfan1776@gmail.com>
2015-10-13 17:13:26 +1100

    more activity cells

---
+++ Tinstagram/commentActivityCell.swift
@@ -0,0 +1,13 @@
+//
+//  commentActivityCell.swift
+//  Tinstagram
+//
+//  Created by Lu Fan on 13/10/2015.
+//  Copyright (c) 2015 Thanh Pham. All rights reserved.
+//
+
+import UIKit
+
+class commentActivityCell: UITableViewCell {
+
+}
