bbae35364b9791c13825e3f0c4f85b2dd32cc034
woowhoo <youngwjung@gmail.com>
2015-10-13 13:20:53 +1100

    Grid
    Grid ddd

--- Tinstagram/GridView.swift
+++ Tinstagram/GridView.swift
@@ -16,16 +16,75 @@ class GridView: UIView {
     // This is a sample. It draws a red line. Please create an Grid.
     
     override func drawRect(rect: CGRect) {
-        // Code HERE !!!
-        let context = UIGraphicsGetCurrentContext()
-        CGContextSetLineWidth(context, 3.0)
-        CGContextSetStrokeColorWithColor(context, UIColor.redColor().CGColor)
+        let gridwidth: CGFloat = 1
         
-        CGContextMoveToPoint(context, 30, 30)
-        CGContextAddLineToPoint(context, 150, 320)
+        var columns : Int
         
-        CGContextStrokePath(context)
+        columns = 3
+        
+        var rows : Int
+        
+        rows = 3
+        
+        
+        
+        let context : CGContextRef = UIGraphicsGetCurrentContext()
+        
+        CGContextSetLineWidth(context, gridwidth)
+        
+        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+        
+        
+        
+        let columnWidth : CGFloat = self.frame.size.width / (CGFloat(columns))
+        
+        let rowHeight : CGFloat = self.frame.size.height / (CGFloat(rows) + 1.0)
+        
+        
+        
+        
+        
+        for i in 1...columns-1{
+            
+            var startPoint: CGPoint = CGPoint(x: columnWidth * CGFloat(i), y: 0.0)
+            
+            var endPoint : CGPoint = CGPoint(x:startPoint.x, y:self.frame.size.height)
+            
+            
+            
+            CGContextMoveToPoint(context, startPoint.x, startPoint.y)
+            
+            CGContextAddLineToPoint(context, endPoint.x, endPoint.y)
+            
+            CGContextStrokePath(context)
+            
+            
+            
+        }
+        
+        
+        
+        for j in 1...rows {
+            
+            var startPoint : CGPoint = CGPoint(x: 0.0, y: rowHeight * CGFloat(j))
+            
+            var endPoint : CGPoint = CGPoint(x: self.frame.size.width, y: startPoint.y)
+            
+            
+            
+            CGContextMoveToPoint(context, startPoint.x, startPoint.y)
+            
+            CGContextAddLineToPoint(context, endPoint.x, endPoint.y)
+            
+            CGContextStrokePath(context)
+            
+        }
+        
+        
+        
+        
+        
     }
     
-
 }
+
